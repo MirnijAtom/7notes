7e1a0615669d2b0319be2d0e0f5a0bf4033c48aa
MirnijAtom <alrein1155@gmail.com>
2025-03-18 19:02:16 +0100

    made a workaround for navigation with sheet. the adding, saving and rest should be adjusted

---
+++ 7notes/CategoryEditView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryEditView.swift
+//  7notes
+//
+//  Created by Aleksandrs Bertulis on 18.03.25.
+//
+
+import SwiftUI
+
+struct CategoryEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryEditView()
+}
