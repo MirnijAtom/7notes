8c0895146912000f48b872a3ba4920e6604bb7c9
MirnijAtom <alrein1155@gmail.com>
2025-03-19 16:03:57 +0100

    Before cleaning the code with chat

---
+++ newNoteInCategoryView.swift
@@ -0,0 +1,18 @@
+//
+//  newNoteInCategoryView.swift
+//  7notes
+//
+//  Created by Aleksandrs Bertulis on 19.03.25.
+//
+
+import SwiftUI
+
+struct newNoteInCategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    newNoteInCategoryView()
+}
